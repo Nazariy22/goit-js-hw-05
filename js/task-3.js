cffb6b45e2e4534a46aca1bfb30a6c58b1265d7e
Nazariy22 <nazariy.vintoniak@gmail.com>
2023-12-10 19:53:16 +0200

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,90 @@
-const profile = {
-  username: 'Jacob',
-  playTime: 300,
-  getInfo: function () {
-    return `${this.username} has ${this.playTime} active hours!`;
-  },
-  changeUsername: function (newName) {
-    this.username = newName;
-  },
-  updatePlayTime: function (hours) {
-    this.playTime += hours;
-  },
+const sortByDescendingFriendCount = users => {
+  return users.sort((a, b) => b.friends.length - a.friends.length);
 };
 
-console.log(profile.getInfo());
+console.log(
+  sortByDescendingFriendCount([
+    {
+      name: 'Moore Hensley',
+      friends: ['Sharron Pace'],
+      gender: 'male',
+    },
+    {
+      name: 'Sharlene Bush',
+      friends: ['Briana Decker', 'Sharron Pace'],
+      gender: 'female',
+    },
+    {
+      name: 'Ross Vazquez',
+      friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+      gender: 'male',
+    },
+    {
+      name: 'Elma Head',
+      friends: ['Goldie Gentry', 'Aisha Tran'],
+      gender: 'female',
+    },
+    {
+      name: 'Carey Barr',
+      friends: ['Jordan Sampson', 'Eddie Strong'],
+      gender: 'male',
+    },
+    {
+      name: 'Blackburn Dotson',
+      friends: ['Jacklyn Lucas', 'Linda Chapman'],
+      gender: 'male',
+    },
+    {
+      name: 'Sheree Anthony',
+      friends: ['Goldie Gentry', 'Briana Decker'],
+      gender: 'female',
+    },
+  ])
+);
 
-profile.changeUsername('Marco');
-console.log(profile.getInfo());
+const getTotalBalanceByGender = (users, gender) => {
+  return users
+    .filter(user => user.gender === gender)
+    .reduce((totalBalance, user) => totalBalance + user.balance, 0);
+};
 
-profile.updatePlayTime(20);
-console.log(profile.getInfo());
+const allUsers = [
+  {
+    name: 'Moore Hensley',
+    gender: 'male',
+    balance: 2811,
+  },
+  {
+    name: 'Sharlene Bush',
+    gender: 'female',
+    balance: 3821,
+  },
+  {
+    name: 'Ross Vazquez',
+    gender: 'male',
+    balance: 3793,
+  },
+  {
+    name: 'Elma Head',
+    gender: 'female',
+    balance: 2278,
+  },
+  {
+    name: 'Carey Barr',
+    gender: 'male',
+    balance: 3951,
+  },
+  {
+    name: 'Blackburn Dotson',
+    gender: 'male',
+    balance: 1498,
+  },
+  {
+    name: 'Sheree Anthony',
+    gender: 'female',
+    balance: 2764,
+  },
+];
+
+console.log(getTotalBalanceByGender(allUsers, 'male'));
+console.log(getTotalBalanceByGender(allUsers, 'female'));
