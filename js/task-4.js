a9d64e88bf1fe8e26cee378824a40ce36ebe5bcc
Nazariy22 <nazariy.vintoniak@gmail.com>
2023-12-10 20:55:41 +0200

    done

--- js/task-4.js
+++ js/task-4.js
@@ -4,43 +4,44 @@ const getTotalBalanceByGender = (users, gender) => {
     .reduce((totalBalance, user) => totalBalance + user.balance, 0);
 };
 
-const allUsers = [
+const Users = [
   {
-    name: 'Moore Hensley',
-    gender: 'male',
-    balance: 2811,
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
   },
   {
-    name: 'Sharlene Bush',
-    gender: 'female',
-    balance: 3821,
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
   },
   {
-    name: 'Ross Vazquez',
-    gender: 'male',
-    balance: 3793,
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
   },
   {
-    name: 'Elma Head',
-    gender: 'female',
-    balance: 2278,
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
   },
   {
-    name: 'Carey Barr',
-    gender: 'male',
-    balance: 3951,
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
   },
   {
-    name: 'Blackburn Dotson',
-    gender: 'male',
-    balance: 1498,
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
   },
   {
-    name: 'Sheree Anthony',
-    gender: 'female',
-    balance: 2764,
-  },
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
 ];
 
-console.log(getTotalBalanceByGender(allUsers, 'male'));
-console.log(getTotalBalanceByGender(allUsers, 'female'));
+console.log(getTotalBalanceByGender(Users, "male")); 
+console.log(getTotalBalanceByGender(Users, "female")); 
+
